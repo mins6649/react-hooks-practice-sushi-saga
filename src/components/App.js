863f3cb044f947acf1b197c67c54cbd95d0fbfe5
minyoungshin <mins6649@gmail.com>
2023-03-01 15:46:21 -0500

    render and maniuplate array

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushiList, setSushiList] = useState([])
+  useEffect(() =>{
+    fetch(API)
+    .then(res => res.json())
+    .then(data => setSushiList(data))
+  }, [])
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushiList={sushiList}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({sushiList}) {
+
+  function handleClick(){
+    console.log('clicked!')
+  }
+  const renderSushiList = sushiList.map((sushi)=>{
+    return <Sushi key={sushi.id} name={sushi.name} img={sushi.img_url} price={sushi.price} handleClick={handleClick}/>
+  })
+  //only display 4, i want a variable that is initialized as (0,4) and everytime button is clicked, we add 4 to the variable
+
+  const [index, setIndex] = useState(0)
+  let sushiDisplay = renderSushiList.slice(index, index+4)
+  function handleMoreSushi(){
+    setIndex((prev)=> prev + 4)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiDisplay}
+      <MoreButton handleMoreSushi={handleMoreSushi}/>
     </div>
   );
 }
